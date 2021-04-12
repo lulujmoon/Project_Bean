95c763913ee3801baa0b900b91151f873f3741f0
thunder-e <pinkpear0114@naver.com>
2021-04-12 19:28:05 +0900

    DTO

---
+++ src/main/java/com/bb/bean/member/MemberController.java
@@ -0,0 +1,10 @@
+package com.bb.bean.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member/**")
+public class MemberController {
+
+}
---
+++ src/main/java/com/bb/bean/member/MemberDAO.java
@@ -0,0 +1,10 @@
+package com.bb.bean.member;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDAO {
+	
+	
+
+}
