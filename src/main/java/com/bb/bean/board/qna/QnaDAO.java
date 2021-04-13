736ee612d618f71d2762c3b1aa698f7a9368c1f1
fly5824 <fly5148@naver.com>
2021-04-13 18:02:55 +0900

    Update QnaDAO.java

--- src/main/java/com/bb/bean/board/qna/QnaDAO.java
+++ src/main/java/com/bb/bean/board/qna/QnaDAO.java
@@ -2,43 +2,44 @@ package com.bb.bean.board.qna;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.bb.bean.board.BoardDAO;
 import com.bb.bean.board.BoardDTO;
 
+@Repository
 public class QnaDAO implements BoardDAO{
+	
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE="com.bb.bean.board.qna.QnaDAO.";
 
 	@Override
 	public List<BoardDTO> getList() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return sqlSession.selectList(NAMESPACE+"getList");
 	}
 
 	@Override
 	public BoardDTO getSelect(BoardDTO boardDTO) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return sqlSession.selectOne(NAMESPACE+"getSelect",boardDTO);
 	}
 
 	@Override
 	public int setInsert(BoardDTO boardDTO) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		return sqlSession.insert(NAMESPACE+"setInsert",boardDTO);
 	}
 
 	@Override
 	public int setUpdate(BoardDTO boardDTO) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		return sqlSession.update(NAMESPACE+"setUpdate",boardDTO);
 	}
 
 	@Override
 	public int setDelete(BoardDTO boardDTO) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		return sqlSession.delete(NAMESPACE+"setDelete",boardDTO);
 	}
-	
-	
-	
 	
 
 }
