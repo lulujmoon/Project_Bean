4c6417f21bf0050259acb00f87b7a77b01fa76a9
fly5824 <fly5148@naver.com>
2021-04-14 10:10:07 +0900

    con setting

--- src/main/java/com/bb/bean/board/faq/FaqController.java
+++ src/main/java/com/bb/bean/board/faq/FaqController.java
@@ -1,8 +1,12 @@
 package com.bb.bean.board.faq;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class FaqController {
+	
+	@Autowired
+	private FaqService faqService;
 
 }
