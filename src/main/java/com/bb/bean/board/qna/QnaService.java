1dbf54782130e5760e4767f1e19410ca762cd6cf
fly5824 <fly5148@naver.com>
2021-04-14 10:06:03 +0900

    service

--- src/main/java/com/bb/bean/board/qna/QnaService.java
+++ src/main/java/com/bb/bean/board/qna/QnaService.java
@@ -2,6 +2,7 @@ package com.bb.bean.board.qna;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bb.bean.board.BoardDTO;
@@ -9,34 +10,32 @@ import com.bb.bean.board.BoardService;
 
 @Service
 public class QnaService implements BoardService {
+	
+	@Autowired
+	private QnaDAO qnaDAO;
 
 	@Override
 	public List<BoardDTO> getList() throws Exception {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public BoardDTO getSelect(BoardDTO boardDTO) throws Exception {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public int setInsert(BoardDTO boardDTO) throws Exception {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int setUpdate(BoardDTO boardDTO) throws Exception {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int setDelete(BoardDTO boardDTO) throws Exception {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
