a2ab67f78285792e706c096f7ed7a028d967249d
lulujmoon <80494527+lulujmoon@users.noreply.github.com>
2021-04-15 15:50:07 +0900

    Update FileManager.java

---
+++ src/main/java/com/bb/bean/util/FileManager.java
@@ -0,0 +1,48 @@
+package com.bb.bean.util;
+
+import java.io.File;
+import java.util.UUID;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileManager {
+
+	//파일 저장
+	public String save(String name, MultipartFile multipartFile, HttpSession session) throws Exception {
+		
+		String path = session.getServletContext().getRealPath("resources/upload/"+name);
+		System.out.println(path);
+		File file = new File(path);
+		if(!file.exists()) {
+			file.mkdirs();
+		}
+		
+		String fileName="";
+		fileName = UUID.randomUUID().toString()+"_"+multipartFile.getOriginalFilename();
+		
+		file = new File(file, fileName);
+		multipartFile.transferTo(file);
+		
+		
+		return fileName;
+	}
+	
+	
+	//파일 삭제
+	public boolean delete(String name, String fileName, HttpSession session) throws Exception {
+		
+		String path = session.getServletContext().getRealPath("resources/upload/"+name);
+		File file = new File(path, fileName);
+		boolean check = false;
+		
+		if(file.exists()) {
+			check = file.delete();
+		}
+		return check;
+	}
+}
