66b31003dd734f43e33813aeb51824592c5d21ec
lulujmoon <80494527+lulujmoon@users.noreply.github.com>
2021-04-23 16:02:23 +0900

    update

---
+++ src/main/webapp/resources/jquery/order.js
@@ -0,0 +1,109 @@
+/**
+ * 
+ */
+ 
+ 
+
+/* 우편번호 API */
+
+$("#popup").click(function(){
+    new daum.Postcode({
+        oncomplete: function(data) {
+            // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+            // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+            // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+            var addr = ''; // 주소 변수
+            var extraAddr = ''; // 참고항목 변수
+
+            //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+            if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                addr = data.roadAddress;
+            } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                addr = data.jibunAddress;
+            }
+
+            // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+            if(data.userSelectedType === 'R'){
+                // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+                // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+                if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                    extraAddr += data.bname;
+                }
+                // 건물명이 있고, 공동주택일 경우 추가한다.
+                if(data.buildingName !== '' && data.apartment === 'Y'){
+                    extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                }
+                // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+                if(extraAddr !== ''){
+                    extraAddr = ' (' + extraAddr + ')';
+                }
+                // 조합된 참고항목을 해당 필드에 넣는다.
+                document.getElementById("addr").value = extraAddr;
+            
+            } else {
+                document.getElementById("addr").value = '';
+            }
+
+            // 우편번호와 주소 정보를 해당 필드에 넣는다.
+            document.getElementById('postcode').value = data.zonecode;
+            document.getElementById("addr").value = addr;
+            // 커서를 상세주소 필드로 이동한다.
+            document.getElementById("addr").focus();
+        }
+    }).open();
+});
+
+
+
+/* 포인트 조회 및 사용 */
+
+
+
+/* 결제하기 */
+
+$("#order-btn").click(function(){
+
+		var IMP = window.IMP; // 생략가능
+		IMP.init('imp36227628'); // 'iamport' 대신 부여받은 "가맹점 식별코드"를 사용
+	
+		let amount = parseInt($("#final").text());
+		let buyer_name = $("#buyerName").val();
+		let buyer_tel = $("#buyerTel").val();
+		let buyer_addr = $("#addr").val();
+		let buyer_postcode = $("#postcode").val();
+
+		IMP.request_pay({
+	    pg : 'inicis', // version 1.1.0부터 지원.
+	    pay_method : 'card',
+	    merchant_uid : 'merchant_' + new Date().getTime(),
+	    name : '주문명:결제테스트',
+	    amount : 20,
+	    buyer_email : '',
+	    buyer_name : buyer_name,
+	    buyer_tel : buyer_tel,
+	    buyer_addr : buyer_addr,
+	    buyer_postcode : buyer_postcode,
+	    m_redirect_url : 'http://localhost/bean/'
+	}, function(rsp) {
+	    if ( rsp.success ) {
+	        var msg = '결제가 완료되었습니다.';
+	        msg += '고유ID : ' + rsp.imp_uid;
+	        msg += '상점 거래ID : ' + rsp.merchant_uid;
+	        msg += '결제 금액 : ' + rsp.paid_amount;
+	        msg += '카드 승인번호 : ' + rsp.apply_num;
+	        
+	        $.post("../order/orderCheck", {
+				amount:20,
+				imp_uid:rsp.imp_uid
+			}, function(result){
+				alert(result);
+				loacation.href="../";
+			})
+	    } else {
+	        var msg = '결제에 실패하였습니다.';
+	        msg += '에러내용 : ' + rsp.error_msg;
+	    }
+	    alert(msg);
+	});
+})
