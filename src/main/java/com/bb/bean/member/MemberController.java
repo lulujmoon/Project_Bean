b59fd676eff2ea6132bdd3748f34cdca1799813a
thunder-e <pinkpear0114@naver.com>
2021-04-28 11:03:39 +0900

    commit

--- src/main/java/com/bb/bean/member/MemberController.java
+++ src/main/java/com/bb/bean/member/MemberController.java
@@ -1,28 +1,16 @@
 package com.bb.bean.member;
 
-import java.io.IOException;
-import java.net.http.HttpHeaders;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 @RequestMapping("/member/**")
@@ -34,74 +22,7 @@ public class MemberController {
 	
 	
 
-	
-	/*
-	@GetMapping("auth/kakao/callback")
-	public @ResponseBody String kakaoCallback(String code) throws Exception { // Data를 리턴해주는 컨트롤러 함수
 
-		// POST방식으로 key=value 데이터 요청 (카카오쪽으로)
-
-		// HttpHeader 오브젝트 생성
-		RestTemplate rt = new RestTemplate();
-		org.springframework.http.HttpHeaders headers = new org.springframework.http.HttpHeaders();
-		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-		// HttpBody 오브젝트 생성
-		MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
-		params.add("grant_type", "authorization_code");
-		params.add("client_id", "8ff76055d95974b4011e5f3f9ce21531");
-		params.add("redirect_uri", "http://localhost/bean/member/auth/kakao/callback");
-		params.add("code", code);
-
-		// HttpHeader와 HttpBody를 하나의 오브젝트에 담기
-		HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<MultiValueMap<String, String>>(
-				params, headers);
-
-		// Http 요청하기 - Post방식으로 - 그리고 response 변수의 응답 받음
-		ResponseEntity<String> response = rt.exchange("https://kauth.kakao.com/oauth/token", HttpMethod.POST,
-				kakaoTokenRequest, String.class);
-
-		// Gson, Json Simple, ObjectMapper
-		ObjectMapper objectMapper = new ObjectMapper();
-		OAuthToken oauthToken = null;
-		try {
-			oauthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
-		} catch (JsonParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		System.out.println("카카오 엑세스 토큰 : " + oauthToken.getAccess_token());
-
-		// HttpHeader 오브젝트 생성
-		RestTemplate rt2 = new RestTemplate();
-		org.springframework.http.HttpHeaders headers2 = new org.springframework.http.HttpHeaders();
-		headers2.add("Authorization", "Bearer " + oauthToken.getAccess_token());
-		headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-		// HttpHeader와 HttpBody를 하나의 오브젝트에 담기
-
-		 HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest = 
-		 new HttpEntity<>(headers2);
-
-		 
-		 // Http 요청하기 - Post방식으로 - 그리고 response 변수의 응답 받음 
-		 ResponseEntity<String> response2 = rt2.exchange( 
-				 "http://kapi.kakao.com/v2/user/me",
-				 HttpMethod.POST,
-				 kakaoProfileRequest, 
-				 String.class 
-				 );	
-
-		return response2.getBody();
-	}
-	*/
 
 	@GetMapping("memberQna")
 	public void memberQna() throws Exception {
