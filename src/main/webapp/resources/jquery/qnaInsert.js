695788eb6de585baa8986188cfd4e3706a104155
fly5824 <fly5148@naver.com>
2021-05-04 11:03:59 +0900

    return 에러

--- src/main/webapp/resources/jquery/qnaInsert.js
+++ src/main/webapp/resources/jquery/qnaInsert.js
@@ -4,35 +4,39 @@
 
 $("#btn").click(function(){
 	
-	var title = document.getElementById("title")
-	var contents = document.getElementById("contents")
-	var category = document.getElementById("category")
-	var con = documenet.getElementById("con")
+	var title = $("#title").val();
+	var con = $("#con").val();
+	var contents = $("#contents").val();
+	var category = $("#category").val();
+
 	
-	if(title.value==""){
-		alert("제목을 입력하세요")
-		title.focus()
-		return false
+	if(title==""){
+		alert("제목을 입력하세요");
+		title.focus();
+		return false;
 	}
 	
-		if(con.value==""){
-		alert("숫자6자리 비밀번호를 입력하세요")
-		con.focus()
-		return false
+		if(con==""){
+		alert("숫자6자리 비밀번호를 입력하세요");
+		con.focus();
+		return false;
 	}
 	
-		if(contents.value==""){
-		alert("내용을 입력하세요")
-		contents.focus()
-		return false
+		if(contents==""){
+		alert("내용을 입력하세요");
+		contents.focus();
+		return false;
 	}
-		if(category.value==""){
-		alert("카테고리를 선택하세요")
-		category.focus()
-		return false
+		if(category==""){
+		alert("카테고리를 선택하세요");
+		category.focus();
+		return false;
 	}
 	
 });
+
+
+
 
 	$("#delete").click(function(){
 		if(confirm("삭제하시겠습니까?")){
@@ -41,3 +45,5 @@ $("#btn").click(function(){
 		return false;
 		}
 	});
+	
+	
